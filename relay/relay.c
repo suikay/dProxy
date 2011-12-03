56682e123fffa8dd6f8ba7c2cbf6b407ee86ce5d
suikay <immozero@gmail.com>
2011-12-03 22:18:48 +0800

    add some tools and the arch of delay

--- relay/relay.c
+++ relay/relay.c
@@ -1,11 +1,16 @@
+#define MAXPENDING	100
 #include <stdio.h> 
 #include <getopt.h>
+#include <event.h>
 
+#include "../include/tools.h"
 /*
  * TODO some tools to add
+ * session
  * buffer, warn, exit
  */
 int source_port, target_port;
+struct event_base *base = event_base_new();
 
 int print_help(char* prog_name)
 {
@@ -20,6 +25,7 @@ int init()
 
 	return 0;
 }
+
 int parse_opt(int argc, char* argv[])
 {
 	char opt;
@@ -60,6 +66,49 @@ int parse_opt(int argc, char* argv[])
 
 	return 0;
 }
+int create_server(char* hostname, int port) 
+{
+	int sock;
+	struct sockaddr_in srv_addr;
+	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		// TODO warn
+		return -1;
+	}
+	memset(srv_addr, 0, sizeof(srv_addr));
+	srv_addr.sin_family = AF_INET;
+	srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	srv_addr.sin_port = htons(port); 
+	if (bind(sock, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) < 0) {
+		// TODO warn
+		return -1;
+	}
+	if (listed(sock, MAXPENDING) < 0) {
+		// TODO warn
+		return -1;
+	}
+	return sock;
+}
+
+void get_connected(evutil_socket_t fd, short what, void *arg)
+{
+	int client_fd;
+	struct sockaddr_in cli_addr;
+	struct session *s;
+
+	unsigned int cli_len = sizeof(cli_addr);
+	if ((client_fd = accept(fd, (struct sockaddr *)&cli_addr, &cli_len)) < 0) {
+		// TODO warn
+		return -1;
+	}
+	s = calloc(sizeof(struct session));	
+	s->cli_fd = client_fd;
+	s->cli_addr = client_fd;
+	s->cli_state = ST_CONNECT;
+	s->srv_state = ST_NOT_CONNECT;
+
+	ev = event_new(base, s->cli_fd, EV_READ | EV_PERSIST, client_read_callback, s);
+	event_add(ev, NULL); 
+}
 
 int main(int argc, char* argv[])
 {
@@ -69,5 +118,13 @@ int main(int argc, char* argv[])
 	if (parse_opt(argc, argv) < 0)
 		return -1;
 
+	if ((sock = create_server(NULL, source_port)) < 0) {
+		// TODO warn
+		return -1;
+	}
+	server_ev = event_new(base, sock, EV_READ | EV_PERSIST, get_connected, NULL);
+	event_add(server_ev, NULL);
+
+	event_base_dispatch(base);
 	return 0;
 }
